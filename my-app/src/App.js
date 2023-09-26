56565d34d21038df058ba89876686ba77e27e0cc
Amirzhanov_Renat <amirzhanov.renat@gmail.com>
2023-09-26 16:48:46 +0400

    ddd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import uuid from 'react-uuid';
 
 function App() {
-	return <div>
-		text
-	</div>;
+  let [name, setName] = useState(false);
+  let [name1, setName1] = useState(false);
+  let [name2, setName2] = useState(false);
+  return (
+    <>
+      <input
+        type="checkbox"
+        onChange={() => {
+          setName(!name);
+        }}
+      />
+      <input
+        type="checkbox"
+        onChange={() => {
+          setName1(!name1);
+        }}
+      />
+      <input
+        type="checkbox"
+        onChange={() => {
+          setName2(!name2);
+        }}
+      />
+      <p>
+        {name ? 'html' : ''} 
+        {name1 ? 'js' : ''} 
+        {name2 ? 'css' : ''}
+      </p>
+    </>
+  );
 }
 export default App;
-
